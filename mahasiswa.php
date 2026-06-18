efe2fd947e591e4a9796c325bfe8720c847b908c
Ghania Hafsha <ghaniahafsha101@gmail.com>
2026-06-18 09:41:24 +0700

    change: php code

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,99 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-     <h1 align="center">
-            WEB TI UNIMUS 2026 OYEE
-        </h1>
-        <table border="1" align="center" cellspacing="0" cellpadding="10px">
-            <tr>
-                <td>
-                    <a href="index.html">Home</a>
-                </td>
-                <td>
-                    <a href="biodata.html">Profile</a>
-                </td>
-                <td>
-                    <a href="kontak.html">contact</a>
-                </td>
-                <td>
-                    <a href="mahasiswa.html">Data Mahasiswa</a>
-                </td>
-            </tr>
-            
-        </table>
-        <h2>
-            Data Mahasiswa
-        </h2>
-      <a href="tambahdata.html">
-        <button>Tambah Data</button>
-    </a>
-        <table border="1" cellpadding="5px">
-            <tr>
-                <th rowspan="2">No</th>
-                <th rowspan="2">Nama</th>
-                <th rowspan="2">Nim</th>
-                <th rowspan="2">Foto</th>
-                <th colspan="3">Nilai</th>
-                <!-- <td>Baris 1, kolom2</td> -->
-            </tr>
-            <tr>
-                <!-- <td>Baris 2, kolom 1</td> -->
-                <th>UTS</th>
-                <th>UAS</th>
-                <th>Tugas</th>
 
-            </tr>
+<body>
+    <h1 align="center">
+        WEB TI UNIMUS 2026 OYEE
+    </h1>
+    <table border="1" align="center" cellspacing="0" cellpadding="10px">
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="biodata.php">Biodata</a>
+            <a href="kontak.php">Kontak</a>
+            <a href="mahasiswa.php">Data Mahasiswa</a>
+        </nav>
+    </table>
+    <h2>
+        Data Mahasiswa
+    </h2>
+    <div class="btn-container">
+        <a href="tambahdata.php" class="btn">+ Tambah Data</a>
+    </div>
+    <table border="1" cellpadding="5px">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+            <th>No. HP</th>
+            <th>Foto</th>
+            <th>Aksi</th>
+
+            <!-- <td>Baris 1, kolom2</td> -->
+        </tr>
         <tr>
             <td align="center">1</td>
-            <td>Ghania Hafsha</td>
-            <td>13242520032</td>
-            <td><img src="asset/img/gambar1.webpgo" width="70px"></td>
-            <td align="center">90</td>
-            <td align="center">85</td>
-            <td align="center">50</td>                          
+            <td>Adelia eka rahmawati</td>
+            <td>13242520036</td>
+            <td align="center">Teknologi Informasi</td>
+            <td align="center">adellia3210@gmail.com</td>
+            <td align="center">0898772537354</td>
+            <td><img src="assets/img/foto.jpg" width="70px"></td>
+            <td>
+                <a href="editdata.php"><button>Edit</button></a>
+                <a href="deletdata.php"><button>Hapus</button></a>
+            </td>
         </tr>
         <tr>
-           <td align="center">2</td>
-           <td>Adelia Eka</td>
-           <td>13242520036</td>
-           <td><img src="asset/img/gambar2.jpg" width="70px"></td>
-           <td align="center">90</td>
-           <td align="">85</td>
-           <td align="center">50</td>
+            <td align="center">2</td>
+            <td>Ghania Hafsha</td>
+            <td>13242520036</td>
+            <td align="center">Teknologi Informasi</td>
+            <td align="">ghania321@gmail.com</td>
+            <td align="center">08983321427</td>
+            <td><img src="assets/img/foto2.jpg" width="70px"></td>
+             <td>
+                <a href="editdata.php"><button>Edit</button></a>
+                <a href="deletdata.php"><button>Hapus</button></a>
+            </td>
         </tr>
-        </table>
-        <hr>
-         <table border="1" cellpadding="5px">
-          <tr>
-                <td>1,1</td>
-                <td>1,2</td>
-                <td>1,3</td>
-                <td>1,4</td>
-            </tr>
-            <tr>
-                <td>2,1</td>
-                <td colspan="2" rowspan="2"></td>
-                <!-- <td>2,3</td> -->
-                <td>2,4</td>
-            </tr>
-            <tr>
-                <td>3,1</td>
-                <!-- <td>3,2</td> -->
-                <!-- <td>3,3</td> -->
-                <td>3,4</td>
-            </tr>
-            <tr>
-                <td>4,1</td>
-                <td>4,2</td>
-                <td>4,3</td>
-                <td>4,4</td>
-            </tr>
-        </table>
-    
+    </table>
+    <hr>
+    <table border="1" cellpadding="5px">
+        <tr>
+            <td>1,1</td>
+            <td>1,2</td>
+            <td>1,3</td>
+            <td>1,4</td>
+        </tr>
+        <tr>
+            <td>2,1</td>
+            <td colspan="2" rowspan="2"></td>
+            <!-- <td>2,3</td> -->
+            <td>2,4</td>
+        </tr>
+        <tr>
+            <td>3,1</td>
+            <!-- <td>3,2</td> -->
+            <!-- <td>3,3</td> -->
+            <td>3,4</td>
+        </tr>
+        <tr>
+            <td>4,1</td>
+            <td>4,2</td>
+            <td>4,3</td>
+            <td>4,4</td>
+        </tr>
+    </table>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+
 </body>
+
 </html>
